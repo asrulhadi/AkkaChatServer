bd7cba6cd24c1821921166dfdb85ead36a7b61c1
asrulhadi <asrulhadi@teknomatrik.com.my>
2021-02-02 13:11:26 +0800

    Commit source code

---
+++ ChatClient/Program.cs
@@ -0,0 +1,134 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Akka.Actor;
+using Akka.Configuration;
+using ChatMessages;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+akka {  
+    actor {
+        provider = remote
+    }
+    remote {
+        dot-netty.tcp {
+		    port = 0
+		    hostname = localhost
+        }
+    }
+}
+");
+            string user = args.Length > 0 ? args[0] : "Dev2";
+            Console.WriteLine($"Using username {user}");
+            using (var system = ActorSystem.Create("MyClient", config))
+            {
+                var chatClient = system.ActorOf(Props.Create<ChatClientActor>());
+                var connectRequest = new ConnectRequest()
+                {
+                    Username = user,
+                };
+                try
+                {
+                    chatClient.Tell(connectRequest);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    if (input.StartsWith("/"))
+                    {
+                        var parts = input.Split(' ');
+                        var cmd = parts[0].ToLowerInvariant();
+                        var rest = string.Join(" ", parts.Skip(1));
+
+                        if (cmd == "/nick")
+                        {
+                            chatClient.Tell(new NickRequest
+                            {
+                                NewUsername = rest
+                            });
+                        }
+                        if (cmd == "/exit")
+                        {
+                            Console.WriteLine("exiting");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        chatClient.Tell(new SayRequest()
+                        {
+                            Text = input,
+                        });
+                    }
+                }
+
+                system.Terminate().Wait();
+            }
+        }
+    }
+
+    class ChatClientActor : ReceiveActor, ILogReceive
+    {
+        private string _nick = "Roggan";
+        private readonly ActorSelection _server = Context.ActorSelection("akka.tcp://MyServer@10.30.12.13:8081/user/ChatServer");
+
+        public ChatClientActor()
+        {
+            Receive<ConnectRequest>(cr =>
+            {
+                Console.WriteLine("Connecting....");
+		_nick = cr.Username;
+                _server.Tell(cr);
+            });
+
+            Receive<ConnectResponse>(rsp =>
+            {
+                Console.WriteLine("Connected!");
+                Console.WriteLine(rsp.Message);
+            });
+
+            Receive<NickRequest>(nr =>
+            {
+                nr.OldUsername = _nick;
+                Console.WriteLine("Changing nick to {0}", nr.NewUsername);
+                _nick = nr.NewUsername;
+                _server.Tell(nr);
+            });
+
+            Receive<NickResponse>(nrsp =>
+            {
+                Console.WriteLine("{0} is now known as {1}", nrsp.OldUsername, nrsp.NewUsername);
+            });
+
+            Receive<SayRequest>(sr =>
+            {
+                sr.Username = _nick;
+                _server.Tell(sr);
+            });
+
+            Receive<SayResponse>(srsp =>
+            {
+                Console.WriteLine("{0}: {1}", srsp.Username, srsp.Text);
+            });
+        }
+    }
+}
+
---
+++ ChatServer/Program.cs
@@ -0,0 +1,82 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Akka.Actor;
+using Akka.Configuration;
+using ChatMessages;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+akka {  
+    actor {
+        provider = remote
+    }
+    remote {
+        dot-netty.tcp {
+            port = 8081
+            hostname = 0.0.0.0
+            public-hostname = 10.30.12.13
+        }
+    }
+}
+");
+
+            using (var system = ActorSystem.Create("MyServer", config))
+            {
+                system.ActorOf(Props.Create(() => new ChatServerActor()), "ChatServer");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class ChatServerActor : ReceiveActor, ILogReceive
+    {
+        private readonly HashSet<IActorRef> _clients = new HashSet<IActorRef>();
+
+        public ChatServerActor()
+        {
+            Receive<SayRequest>(message =>
+            {
+                var response = new SayResponse
+                {
+                    Username = message.Username,
+                    Text = message.Text,
+                };
+                foreach (var client in _clients) client.Tell(response, Self);
+            });
+
+            Receive<ConnectRequest>(message =>
+            {
+                _clients.Add(Sender);
+                Sender.Tell(new ConnectResponse
+                {
+                    Message = "Hello and welcome to Akka.NET chat example",
+                }, Self);
+            });
+
+            Receive<NickRequest>(message =>
+            {
+                var response = new NickResponse
+                {
+                    OldUsername = message.OldUsername,
+                    NewUsername = message.NewUsername,
+                };
+
+                foreach (var client in _clients) client.Tell(response, Self);
+            });
+        }
+    }
+}
+
